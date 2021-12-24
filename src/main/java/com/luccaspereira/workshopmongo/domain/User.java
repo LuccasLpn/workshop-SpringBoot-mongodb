cf61dc35f8f5cf81c35f21d081c784e321dd6cb6
Luccas Pereira <Luccas.lpn@outlook.com>
2021-12-24 01:20:53 -0300

    Operação de instanciação da base de dados

--- src/main/java/com/luccaspereira/workshopmongo/domain/User.java
+++ src/main/java/com/luccaspereira/workshopmongo/domain/User.java
@@ -21,11 +21,11 @@ public class User implements Serializable{
 	public User() {
 	}
 
-	public User(String name, String email, String id) {
+	public User(String id,String name, String email) {
 		super();
+		this.id = id;
 		this.name = name;
 		this.email = email;
-		this.id = id;
 	}
 
 
