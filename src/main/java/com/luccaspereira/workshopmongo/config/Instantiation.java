927c1a7127589952c2c8ae185ad304bdf7a18287
Luccas Pereira <Luccas.lpn@outlook.com>
2021-12-24 12:37:57 -0300

    Referenciando os posts do usuário

--- src/main/java/com/luccaspereira/workshopmongo/config/Instantiation.java
+++ src/main/java/com/luccaspereira/workshopmongo/config/Instantiation.java
@@ -38,9 +38,11 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo abraços", new AuthorDTO(maria));
 		Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei Feliz Hoje",new AuthorDTO(maria));
-		
-		
+	
 		postRepository.saveAll(Arrays.asList(p1,p2));
+		
+		maria.getPosts().addAll(Arrays.asList(p1,p2));
+		repository.save(maria);
 
 	}
 
